05c16d40d56ec6c61733f8e0ec5fea61808558d6
Nixken <161881576+Nixken463@users.noreply.github.com>
2024-12-28 22:12:12 +0100

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-import customtkinter as ctk
-
-class settings(ctk.CTkFrame):
-    def __init__(self, parent):
-
-        super().__init__(parent)
-        ctk.CTkLabel(self, text="Settings").pack()
-        ctk.CTkButton(self, text="Back to main", command=lambda: parent.show_page("main")).pack()
-
